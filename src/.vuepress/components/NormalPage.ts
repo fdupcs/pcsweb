24ce336078e28e31be298e138f2f8b66ad777b4e
Alex6357 <73743056+Alex6357@users.noreply.github.com>
2025-04-09 15:37:34 +0800

    feat: 评论区动态加载

---
+++ src/.vuepress/components/NormalPage.ts
@@ -0,0 +1,132 @@
+import type { HeaderLevels } from "@vuepress/helper/client";
+import { hasGlobalComponent } from "@vuepress/helper/client";
+import type { ComponentOptions, SlotsType, VNode } from "vue";
+import {
+  computed,
+  defineComponent,
+  h,
+  ref,
+  resolveComponent,
+  onMounted,
+} from "vue";
+import { usePageFrontmatter, withBase } from "vuepress/client";
+import { RenderDefault } from "vuepress-shared/client";
+
+import BreadCrumb from "vuepress-theme-hope/components/BreadCrumb.js";
+import MarkdownContent from "vuepress-theme-hope/components/MarkdownContent.js";
+import PageNav from "vuepress-theme-hope/components/PageNav.js";
+import PageTitle from "vuepress-theme-hope/components/PageTitle.js";
+import { useThemeLocaleData } from "vuepress-theme-hope/composables/index.js";
+import PageMeta from "vuepress-theme-hope/modules/info/components/PageMeta.js";
+import TOC from "vuepress-theme-hope/modules/info/components/TOC.js";
+import { useDarkMode } from "vuepress-theme-hope/modules/outlook/composables/index.js";
+
+import type { ThemeNormalPageFrontmatter } from "../../../node_modules/vuepress-theme-hope/lib/shared/index.js";
+
+import "../../../node_modules/vuepress-theme-hope/lib/client/styles/normal-page.scss";
+
+export default defineComponent({
+  name: "NormalPage",
+
+  slots: Object as SlotsType<{
+    top?: () => VNode[] | VNode | null;
+    bottom?: () => VNode[] | VNode | null;
+
+    contentBefore?: () => VNode[] | VNode | null;
+    contentAfter?: () => VNode[] | VNode | null;
+
+    tocBefore?: () => VNode[] | VNode | null;
+    tocAfter?: () => VNode[] | VNode | null;
+  }>,
+
+  setup(_props, { slots }) {
+    const frontmatter = usePageFrontmatter<ThemeNormalPageFrontmatter>();
+    const { isDarkMode } = useDarkMode();
+    const themeLocale = useThemeLocaleData();
+
+    const tocEnable = computed(
+      () => frontmatter.value.toc ?? themeLocale.value.toc ?? true
+    );
+
+    const headerLevels = computed<HeaderLevels>(() => [
+      2,
+      (frontmatter.value.headerDepth ?? themeLocale.value.headerDepth ?? 2) + 1,
+    ]);
+
+    const canLoadComment = ref(false);
+
+    async function checkCanLoadComment() {
+      if (hasGlobalComponent("CommentService")) {
+        try {
+          const res = await fetch(
+            "https://waline.fdupcs.top/api/comment?type=count"
+          );
+          if (res.ok) canLoadComment.value = true;
+        } catch (e) {
+          // console.log(e);
+        }
+      }
+    }
+    onMounted(() => {
+      checkCanLoadComment();
+    });
+
+    return (): VNode =>
+      h(
+        "main",
+        { id: "main-content", class: "vp-page" },
+        h(
+          hasGlobalComponent("LocalEncrypt")
+            ? (resolveComponent("LocalEncrypt") as ComponentOptions)
+            : RenderDefault,
+          () => [
+            slots.top?.(),
+            frontmatter.value.cover
+              ? h(
+                  "div",
+                  { class: "page-cover" },
+                  h("img", {
+                    src: withBase(frontmatter.value.cover),
+                    alt: "",
+                    "no-view": "",
+                  })
+                )
+              : null,
+            h(BreadCrumb),
+            h(PageTitle),
+            tocEnable.value
+              ? h(
+                  TOC,
+                  {
+                    options: {
+                      levels: headerLevels.value,
+                      ignore: [".vp-badge"],
+                    },
+                  },
+                  {
+                    before: slots.tocBefore,
+                    after: slots.tocAfter,
+                  }
+                )
+              : null,
+            h(
+              MarkdownContent,
+              {},
+              {
+                before: slots.contentBefore,
+                after: slots.contentAfter,
+              }
+            ),
+            h(PageMeta),
+            h(PageNav),
+            canLoadComment.value
+              ? h(resolveComponent("CommentService"), {
+                  darkmode: isDarkMode.value,
+                })
+              : null,
+            slots.bottom?.(),
+          ]
+        )
+      );
+  },
+});
--- src/.vuepress/theme.ts
+++ src/.vuepress/theme.ts
@@ -3,86 +3,94 @@ import { hopeTheme } from "vuepress-theme-hope";
 import navbar from "./navbar.js";
 import sidebar from "./sidebar.js";
 
-export default hopeTheme({
-  hostname: "https://fdupcs.top",
+export default hopeTheme(
+  {
+    hostname: "https://fdupcs.top",
 
-  author: {
-    name: "PCS",
-    url: "https://github.com/fdupcs",
-  },
+    author: {
+      name: "PCS",
+      url: "https://github.com/fdupcs",
+    },
 
-  logo: "/assets/icon.png",
+    logo: "/assets/icon.png",
 
-  repo: "fdupcs/pcsweb",
+    repo: "fdupcs/pcsweb",
 
-  docsDir: "src",
+    docsDir: "src",
 
-  navbar,
+    navbar,
 
-  sidebar,
+    sidebar,
 
-  license: "CC BY-NC 4.0",
+    license: "CC BY-NC 4.0",
 
-  favicon: "/assets/icon.png",
+    favicon: "/assets/icon.png",
 
-  footer: "PCS 服务队官方网站",
-  displayFooter: true,
+    footer: "PCS 服务队官方网站",
+    displayFooter: true,
 
-  blog: {
-    description: "PCS 志愿者服务队",
-    avatar: "/assets/logo.png",
-    intro: "/intro.html",
-    medias: {
-      "PCS 服务队公众号": {
-        icon: "https://fdupcs.top/assets/medias/PCS.svg",
-        link: "/contact#pcs-服务队公众号",
+    blog: {
+      description: "PCS 志愿者服务队",
+      avatar: "/assets/logo.png",
+      intro: "/intro.html",
+      medias: {
+        "PCS 服务队公众号": {
+          icon: "https://fdupcs.top/assets/medias/PCS.svg",
+          link: "/contact#pcs-服务队公众号",
+        },
+        "CPS 婚介所公众号": {
+          icon: "https://fdupcs.top/assets/medias/CPS.svg",
+          link: "/contact#cps-婚介所公众号",
+        },
+        "PCService 日常答疑群": {
+          icon: "https://fdupcs.top/assets/medias/QQ.svg",
+          link: "https://qm.qq.com/q/BqsEh9fGi6",
+        },
+        "PCService 频道": {
+          icon: "https://fdupcs.top/assets/medias/QQChannel.svg",
+          link: "https://pd.qq.com/s/ealld0rtu?businessType=5",
+        },
       },
-      "CPS 婚介所公众号": {
-        icon: "https://fdupcs.top/assets/medias/CPS.svg",
-        link: "/contact#cps-婚介所公众号",
+    },
+
+    metaLocales: {
+      editLink: "在 GitHub 上编辑此页",
+    },
+
+    blogLocales: {
+      intro: "服务队简介",
+    },
+
+    contributors: false,
+
+    changelog: false,
+
+    markdown: {
+      hint: true,
+      tabs: true,
+      spoiler: true,
+      figure: true,
+      imgMark: true,
+      imgSize: true,
+      imgLazyload: true,
+      align: true,
+    },
+
+    plugins: {
+      blog: true,
+
+      icon: {
+        assets: "fontawesome-with-brands",
       },
-      "PCService 日常答疑群": {
-        icon: "https://fdupcs.top/assets/medias/QQ.svg",
-        link: "https://qm.qq.com/q/BqsEh9fGi6",
-      },
-      "PCService 频道": {
-        icon: "https://fdupcs.top/assets/medias/QQChannel.svg",
-        link: "https://pd.qq.com/s/ealld0rtu?businessType=5",
+
+      comment: {
+        provider: "Waline",
+        serverURL: "https://waline.fdupcs.top",
+        requiredMeta: ["nick"],
+        reaction: [],
       },
     },
   },
 
-  metaLocales: {
-    editLink: "在 GitHub 上编辑此页",
-  },
-
-  blogLocales: {
-    intro: "服务队简介",
-  },
-
-  markdown: {
-    hint: true,
-    tabs: true,
-    spoiler: true,
-    figure: true,
-    imgMark: true,
-    imgSize: true,
-    imgLazyload: true,
-    align: true,
-  },
-
-  plugins: {
-    blog: true,
-
-    icon: {
-      assets: "fontawesome-with-brands",
-    },
-
-    comment: {
-      provider: "Waline",
-      serverURL: "pcsweb-waline.vercel.app",
-      requiredMeta: ["nick"],
-      reaction: [],
-    },
-  },
-});
+  { custom: true }
+);
